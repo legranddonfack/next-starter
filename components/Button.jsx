a564b6f7246fc4a7440e69499aca31bf7129965d
donfack <donfack@campus.uni-paderbron.de>
2023-05-26 15:00:12 +0200

    Update Button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,19 @@
 
 
-const Button = ({children, onClick, className}) => {
+const Button = ({children, onClick, className, type, bgColor}) => {
+  if (type && type === "icon") {
+    return (
+      <button 
+        onClick={onClick}
+        className={`${className} rounded-[5px] w-[40px] h-[40px] bg-${bgColor}-light hover:bg-${bgColor} active:bg-${bgColor}-light flex items-center justify-center`}>
+        { children }
+      </button>
+    )
+  }
   return (
     <button 
       onClick={onClick}
-      className={className + " rounded-[5px] w-[40px] h-[40px] bg-grey-light hover:bg-grey active:bg-grey-light flex items-center justify-center"}>
+      className={`${className} rounded-[5px] px-[20px] h-[40px] bg-secondary hover:bg-${bgColor}-light active:bg-${bgColor} flex items-center justify-center`}>
       { children }
     </button>
   )
