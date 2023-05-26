8c975d892ad45b80d60586d83eed063de845c706
donfack <donfack@campus.uni-paderbron.de>
2023-05-26 15:00:53 +0200

    Navbar update: includes isUserLoggedIn variable

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,20 +5,24 @@ import BellIcon from "../public/assets/svg/bell.svg"
 import Avatar from "../public/assets/images/profile.png"
 import Button from "@components/Button"
 import Link from "next/link"
+import { useState, useEffect } from "react"
 
-const onProfileClick = () => {
-
-}
-
-const onNotificationsClick = () => {
-
-}
-
-const onAppsClick = () => {
-
-}
 
 const Navbar = () => {
+  const isUserLoogedIn = true;
+
+  const onProfileClick = () => {
+
+  }
+
+  const onNotificationsClick = () => {
+
+  }
+
+  const onAppsClick = () => {
+
+  }
+
   return (
     <header className="header">
       <nav className="nav">
@@ -28,11 +32,26 @@ const Navbar = () => {
           </span>
         </Link>
         <div className="flex flex-row items-end">
-          <Image src={Avatar} alt="Avatar" className="h-[39px] w-[39px] cursor-pointer" onClick={onProfileClick}/>
-          <Button onClick={onNotificationsClick} className="ml-[20px]">
-            <Image src={BellIcon} alt="Notifications" className="h-[20px] w-[20px]"/>
-          </Button>
-          <Button onClick={onAppsClick} className="ml-[20px]">
+
+          {/*Desktop navigation*/}
+
+          {
+            isUserLoogedIn ? (
+              <div className="flex flex-row">
+                <Image src={Avatar} alt="Avatar" className="h-[39px] w-[39px] cursor-pointer" onClick={onProfileClick}/>
+                <Button type="icon" onClick={onNotificationsClick} className="ml-[20px]" bgColor="grey">
+                  <Image src={BellIcon} alt="Notifications" className="h-[20px] w-[20px]"/>
+                </Button>
+              </div>
+            ) : (
+              <Button bgColor="secondary">
+                <span className="text-white font-bold">
+                  Login
+                </span>
+              </Button>
+            )
+          }
+          <Button type="icon" bgColor="grey" onClick={onAppsClick} className="ml-[20px]">
             <Image src={AppsIcon} alt="Apps" className="h-[20px] w-[20px]"/>
           </Button>
         </div>
